2f5f958c6bcb1ad9b5fc42ea2cbff7de21073269
Stephen Kim <stephen@Stephens-MBP.fios-router.home>
2018-12-01 16:33:34 -0500

    temp schanges

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import parselmouth
 import glob
 import os.path
+import numpy as np
+#from sklearn.mixture import GMM
+
+## TODO:
+# load in all audio files
+# figure out how to cut off last sentence for testing (or a small phrase)
+# figure out how to plot mfcc using api
+# visualize gaussians
+    # is it fast enough
+# once all done see if it works live
+
 
 for file in glob.glob("english1.mp3"):
     s = parselmouth.Sound(file)
 
 mfcc = s.to_mfcc()
 features = mfcc.extract_features()
-print features.as_array()
+print features.to_matrix_features()
+#print features.as_array()
