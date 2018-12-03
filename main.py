8652728d26eee195f08bd131b0294961b0e64100
Stephen Kim <stephen@Stephens-MBP.fios-router.home>
2018-12-03 16:31:22 -0500

    percent

--- main.py
+++ main.py
@@ -62,61 +62,73 @@ def plot_results(X, Y_, means, covariances, index, title):
     plt.yticks(())
     plt.title(title)
 
+iterations = 1
+
+for i in range(0, iterations):
+    values = [[0 for i in range(0, 4)] for j in range(0, 1)]
+    targets = []
+    counter = 0
+
+    for fp in glob.iglob("male/*.mp3"):
+        for file in glob.glob(fp):
+            s = parselmouth.Sound(file)
+
+        mfcc = s.to_mfcc()
+        features = mfcc.extract_features()
+        values = np.concatenate((values,np.transpose(features.as_array())), axis=0)
+        target = [counter for i in range(0, len(features))]
+        targets.append(target)
+        counter += 1
+        #[[0 for i in range(0, 4)] for j in range(0, 1)]
+        #print str(len(np.transpose(features.as_array()))) + " male"
+
+    for fp in glob.iglob("female/*.mp3"):
+        for file in glob.glob(fp):
+            s = parselmouth.Sound(file)
+
+        mfcc = s.to_mfcc()
+        features = mfcc.extract_features()
+        values = np.concatenate((values,np.transpose(features.as_array())), axis=0)
+        target = [counter for i in range(0, len(features))]
+        targets.append(target)
+        counter += 1
+        #print str(len(np.transpose(features.as_array()))) + " female"
+
+    ## delete later
+    #iris = datasets.load_iris()
+    #print iris.target
+    #print iris.data
+
+    flat_list = list(itertools.chain.from_iterable(targets))
+    targets = np.array(flat_list)
+
+    values = np.delete(values,0,0)
+    skf = StratifiedKFold(n_splits=4)
+    train_index, test_index = next(iter(skf.split(values, targets)))
+
+    X_train = values[train_index]
+    y_train = targets[train_index]
+    X_test = values[test_index]
+    y_test = targets[test_index]
+
+    # print len(values)
+    # print str(len(test_index)) + " y_test " + str(len(y_test))
+    # print str(len(train_index)) + " y_test " + str(len(y_train))
 
 
-values = [[0 for i in range(0, 4)] for j in range(0, 1)]
-targets = []
-counter = 0
+    n_classes = len(np.unique(y_train))
 
-for fp in glob.iglob("male/*.mp3"):
-    for file in glob.glob(fp):
-        s = parselmouth.Sound(file)
+    gmm = mixture.GaussianMixture(n_components = n_classes, max_iter = 200)
+    trained = gmm.fit(X_train)
+    labels = trained.predict(X_test)
 
-    mfcc = s.to_mfcc()
-    features = mfcc.extract_features()
-    values = np.concatenate((values,np.transpose(features.as_array())), axis=0)
-    target = [counter for i in range(0, len(features))]
-    targets.append(target)
-    counter += 1
-
-    [[0 for i in range(0, 4)] for j in range(0, 1)]
-    #print str(len(np.transpose(features.as_array()))) + " male"
-
-for fp in glob.iglob("female/*.mp3"):
-    for file in glob.glob(fp):
-        s = parselmouth.Sound(file)
-
-    mfcc = s.to_mfcc()
-    features = mfcc.extract_features()
-    values = np.concatenate((values,np.transpose(features.as_array())), axis=0)
-    target = [counter for i in range(0, len(features))]
-    targets.append(target)
-    counter += 1
-    #print str(len(np.transpose(features.as_array()))) + " female"
-
-## delete later
-iris = datasets.load_iris()
-print iris.target
-print iris.data
-
-flat_list = list(itertools.chain.from_iterable(targets))
-targets = np.array(flat_list)
-
-values = np.delete(values,0,0)
-skf = StratifiedKFold(n_splits=4)
-train_index, test_index = next(iter(skf.split(values, targets)))
-
-X_train = values[train_index]
-y_train = targets[train_index]
-X_test = values[test_index]
-y_test = targets[test_index]
-
-n_classes = len(np.unique(y_train))
-
-gmm = mixture.GaussianMixture(n_components = n_classes, max_iter = 200)
-trained = gmm.fit(X_train)
-labels = trained.predict(X_train)
-
+    percent = 0;
+    for j in range(0, (len(labels))):
+        #print str(labels[j]) + " hi " + str(y_test[j])
+        if labels[j] == y_test[j]:
+            percent += 1
+    percent = float(percent) / (len(labels))
+    print percent
 #plot_results(X_train, labels, trained.means_, trained.covariances_, 0,'Gaussian Mixture')
 #plt.figure()
 #plt.subplots_adjust(bottom=.01, top=0.95, hspace=.15, wspace=.05,left=.01, right=.99)
