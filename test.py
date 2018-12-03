81d7ea12484e0dabba78e357c6737686f7c96974
Vincent Song <vsong@Vincents-MBP.fios-router.home>
2018-12-03 16:49:58 -0500

    kasia branch

---
+++ test.py
@@ -0,0 +1,11 @@
+from python_speech_features import mfcc
+from python_speech_features import delta
+from python_speech_features import logfbank
+import scipy.io.wavfile as wav
+
+(rate,sig) = wav.read("male/english1.mp3")
+mfcc_feat = mfcc(sig,rate)
+d_mfcc_feat = delta(mfcc_feat, 2)
+fbank_feat = logfbank(sig,rate)
+
+print(fbank_feat[1:3,:])
