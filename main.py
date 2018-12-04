c6eb12f74383c556db9f307e1b50ee853e877cf4
Stephen Kim <stephen@Stephens-MBP.fios-router.home>
2018-12-04 17:34:05 -0500

    final

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn import mixture
 from sklearn import datasets
 from sklearn.model_selection import StratifiedKFold
+from sklearn import preprocessing
 import itertools
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -65,20 +66,69 @@ def plot_results(X, Y_, means, covariances, index, title):
     plt.title(title)
 
 iterations = 1
+models = {}
 
 for i in range(0, iterations):
-    values = [[0 for i in range(0, 20)] for j in range(0, 1)]
-    targets = []
+    #training
+    targets = np.asarray(())
     counter = 0
+    person = 0
+    values = np.asarray(())
 
-    for wav in glob.iglob("wav_smaller/*/wav/*.wav"):
-        print str(counter) + " is done"
+    for wav in glob.iglob("train/*/wav/*.wav"):
         (rate,audio) = wavfile.read(wav)
         features = mfcc.mfcc(audio, rate, 0.025, 0.01, 20, appendEnergy = True)
-        values = np.concatenate((values, features), axis=0)
-        target = [counter for i in range(0, len(features))]
-        targets.append(target)
+        pfeatures = preprocessing.scale(features)
+        if values.size == 0:
+            values = pfeatures
+        else:
+            values = np.vstack((values, pfeatures))
+        # target = [counter for i in range(0, len(features))]
+        # targets.append(target)
+        # print counter
         counter += 1
+        if counter % 7 == 0 and counter != 0: # can change to match how many train each has
+            gmm = mixture.GaussianMixture(n_components = 16, max_iter = 200)
+            trained = gmm.fit(values)
+            models[person] = trained #store gmm
+            print str(person) + " is done"
+            #print str(values) + "values array"
+            values = np.asarray(()) # reset
+            person += 1 # next person
+
+    #testing
+    targets = np.asarray(())
+    counter = 0
+    person = 0
+    values = np.asarray(())
+    expected = []
+    real = []
+
+    for wav in glob.iglob("test/*/wav/*.wav"):
+        (rate,audio) = wavfile.read(wav)
+        features = mfcc.mfcc(audio, rate, 0.025, 0.01, 20, appendEnergy = True)
+        pfeatures = preprocessing.scale(features)
+        if values.size == 0:
+            values = pfeatures
+        else:
+            values = np.vstack((values, pfeatures))
+        counter += 1
+        if counter % 3 == 0 and counter != 0:
+            log_likelihood = np.zeros(len(models))
+            for i in range(len(models)):
+                gmm = models[i]  #checking each model one by one
+                scores = np.array(gmm.score(values))
+                log_likelihood[i] = scores.sum()
+            likely = np.argmax(log_likelihood)
+            expecte d = np.append(expected, person)
+            real = np.append(real, likely)
+            values = np.asarray(())
+            person += 1
+    print str(expected) + "expected"
+    print str(real) + "real"
+
+
+
 
     # for fp in glob.iglob("male/*.mp3"):
     #     for file in glob.glob(fp):
@@ -103,46 +153,44 @@ for i in range(0, iterations):
     #     target = [counter for i in range(0, len(features))]
     #     targets.append(target)
     #     counter += 1
-        #print str(len(np.transpose(features.as_array()))) + " female"
+          #print str(len(np.transpose(features.as_array()))) + " female"
 
     ## delete later
     #iris = datasets.load_iris()
     #print iris.target
     #print iris.data
 
-    flat_list = list(itertools.chain.from_iterable(targets))
-    targets = np.array(flat_list)
+        # flat_list = list(itertools.chain.from_iterable(targets))
+        # #targets = np.array(flat_list)
+        #
+        # values = np.delete(values,0,0)
+        # skf = StratifiedKFold(n_splits=4)
+        # train_index, test_index = next(iter(skf.split(values, targets)))
+        #
+        # X_train = values[train_index]
+        # y_train = targets[train_index]
+        # X_test = values[test_index]
+        # y_test = targets[test_index]
+        #
+        # # print len(values)
+        # # print str(len(test_index)) + " y_test " + str(len(y_test))
+        # # print str(len(train_index)) + " y_test " + str(len(y_train))
+        #
+        #
+        # n_classes = len(np.unique(y_train))
+        #
+        # gmm = mixture.GaussianMixture(n_components = n_classes, max_iter = 200)
+        # print 'Training...'
+        # trained = gmm.fit(X_train)
+        # print 'done training'
 
-    values = np.delete(values,0,0)
-    skf = StratifiedKFold(n_splits=4)
-    train_index, test_index = next(iter(skf.split(values, targets)))
-
-    X_train = values[train_index]
-    y_train = targets[train_index]
-    X_test = values[test_index]
-    y_test = targets[test_index]
-
-    # print len(values)
-    # print str(len(test_index)) + " y_test " + str(len(y_test))
-    # print str(len(train_index)) + " y_test " + str(len(y_train))
-
-
-    n_classes = len(np.unique(y_train))
-
-    gmm = mixture.GaussianMixture(n_components = n_classes, max_iter = 200)
-    print 'Training...'
-    trained = gmm.fit(X_train)
-    print 'predicting...'
-    labels = trained.predict(X_test)
-    print 'done predicting!'
-
-    percent = 0;
-    for j in range(0, (len(labels))):
-        #print str(labels[j]) + " hi " + str(y_test[j])
-        if labels[j] == y_test[j]:
-            percent += 1
-    percent = float(percent) / (len(labels))
-    print percent
+# percent = 0;
+# for j in range(0, (len(labels))):
+#     #print str(labels[j]) + " hi " + str(y_test[j])
+#     if labels[j] == y_test[j]:
+#         percent += 1
+# percent = float(percent) / (len(labels))
+# print percent
 #plot_results(X_train, labels, trained.means_, trained.covariances_, 0,'Gaussian Mixture')
 #plt.figure()
 #plt.subplots_adjust(bottom=.01, top=0.95, hspace=.15, wspace=.05,left=.01, right=.99)
